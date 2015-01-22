5c06bad4b1a2cfebd32ae479eedbe6d7c207159b
millionhari <mikemhari@gmail.com>
2015-01-21 19:21:47 -0800

    Completed stringifyJSON. Still haven't completed getElementsByClassName.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,40 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+    if (typeof(obj) === "function"){
+      return "null";
+    }
+    if (typeof(obj) === "boolean"){
+      return obj.toString();
+    }
+    if (typeof(obj) === "undefined"){
+      return undefined;
+    }
+    if (obj === null){
+      return "null";
+    }
+    if (typeof(obj) === "number"){
+      return obj.toString();
+    }
+    if (typeof(obj) === "string"){
+      return '"'+obj+'"';
+    }
+    if (Array.isArray(obj)){
+      var bank = [];
+      for (var i = 0 ; i < obj.length ; i++){
+        bank.push(stringifyJSON(obj[i]));
+      }
+      return '['+bank+']';
+    }
+    if (Object.prototype.toString.call(obj) == "[object Object]"){
+      var bankObj = [];
+      for (var x in obj){
+        if (typeof(obj[x]) !== "function" && typeof(x) !== "function" && typeof(obj[x]) !== "undefined"){
+          bankObj.push(stringifyJSON(x) + ":" + stringifyJSON(obj[x]));
+        } else {
+          stringifyJSON();
+        }
+      }
+      return '{'+bankObj+'}';
+    }
 };
