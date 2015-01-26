8889134acc466b8db1311b6a3771b4686d238442
millionhari <mikemhari@gmail.com>
2015-01-25 22:11:17 -0800

    Finished getElementsByClassName. Finished recursion, but need to refactor code to clean up.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,21 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className){
+	var bank = [];
+
+	function lookThrough(element){
+		var children = element.children;
+		for (var i = 0 ; i < children.length ; i++){
+			if(children[i].classList.contains(className)) {
+				bank.push(children[i]);
+			}
+			if(children[i].hasChildNodes) {
+				lookThrough(children[i]);
+			}
+		}
+	}
+	
+	lookThrough(document);
+	return bank;
 };
